972d3e95ea5e15ecbe2240ce081494d49fec4d6b
abdulAlim <alimsujon12@gmail.com>
2024-05-22 03:12:44 +0600

    not found route created

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import { productRoute } from './app/modules/product/product.route';
 import { orderRoute } from './app/modules/Order/Order.route';
+import notFound from './app/middleware/NotFound';
 
 const app: Application = express();
 //parsers
@@ -11,5 +12,7 @@ app.use(cors());
 //enpoints
 app.use('/api', productRoute);
 app.use('/api', orderRoute);
+
+app.use('*', notFound);
 
 export default app;
